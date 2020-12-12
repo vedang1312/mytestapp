927346cf5256150afd2f28ecd5e6aab62500d670
Vedang kinkar <kinkar.vedang13@gmail.com>
2020-12-12 16:40:12 +0530

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main(String args[]){
+        System.out.println("Hello world from app");
+        System.out.println("Hello Vedang");
+      
+    }
+}
